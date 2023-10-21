3b8a26e605803b62d07fda19519f64f12fb274ea
akinremimoses <akinremimoses93@gmail.com>
2023-10-20 17:09:12 -0700

    printf

--- handle_print.c
+++ handle_print.c
@@ -1,68 +1,50 @@
 #include "main.h"
 /**
- * handle_print- prints argument based on format type
- * @format: formatted string
- * @list: argument list
- * @index: index
- * @buffer: buffer array
- * @flags: active flags
- * @precision: specs
- * @size: size specifier
- * @width: width
- * Return: return 1 0r 2 depending on putput
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
  */
-
-int handle_print(const char *format, int *index, va_list list, char buffer[],
-		int flags, int width, int precision, int size)
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
-	format_t format_types[] = {
+	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octal}, {'x',
-
-print_hexadecimal},
-		{'X', print_hexa_upper}, {'p', print_pointer}, {'S',
-print_non_printable},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-	for (i = 0; format_types[i].format != '\0'; i++)
+	if (fmt_types[i].fmt == '\0')
 	{
-		if (format[*index] == format_types[i].format)
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
 		{
-			return (format_types[i].fn(list, buffer, flags, width,
-						precision, size));
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
 		}
-		if (format_types[i].format == '\0')
-		{
-			if (format[*index] == '\0')
-			{
-				return (-1);
-			}
-			unknow_len += write(1, "%%", 1);
-			if (format[*index - 1] == ' ')
-			{
-				unknow_len += write(1, " ", 1);
-			}
-			else if (width)
-			{
-				--(*index);
-				while (format[*index] != ' ' && format[*index] != '%')
-				{
-					--(*index);
-				}
-				if (format[*index] == ' ')
-				{
-					--(*ind);
-				}
-				return (1);
-			}
-			unknow_len += write(1, &format[*index], 1);
-			return (unknow_len);
-		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
-
 	return (printed_chars);
 }
 
--- utils.c
+++ utils.c
@@ -1,102 +1,87 @@
 #include "main.h"
+
 /**
- * is_printable- checks if a char is printable
- * @c: evaluated char
- * Return: 1 if printable else 0
+ * is_printable - Evaluates if a char is printable
+ * @c: Char to be evaluated.
+ *
+ * Return: 1 if c is printable, 0 otherwise
  */
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
-	{
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+
+	return (0);
+}
 
 /**
- * append_hexa_code- Append ascii in hexadecimal code to buffer
- * @buffer: char array
- * @y: index
- * @ascii_code: ASCII 
- * Return: 3;
+ * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * @buffer: Array of chars.
+ * @i: Index at which to start appending.
+ * @ascii_code: ASSCI CODE.
+ * Return: Always 3
  */
-
-int append_hexa_code(char ascii_code, char buffer[], int y)
+int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-
+	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
-	{
 		ascii_code *= -1;
-	}
-	buffer[y++] = '\\';
-	buffer[y++] = 'x';
 
-	buffer[y++] = map_to[ascii_code / 16];
-	buffer[y] = map_to[ascii_code % 16];
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
 	return (3);
 }
-/**
- * is_digit- verifies if char is a digit or not
- * @c: evaluated char
- * Return: 1 if c is digit else return 0
- */
 
+/**
+ * is_digit - Verifies if a char is a digit
+ * @c: Char to be evaluated
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
 int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
-	{
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-}
-/**
- * convert_size_number- casts nuber to specified size
- * @n: casted number
- * @size: type to be casted
- * Return: casted value
- */
-long int convert_size_number(long int n, int size)
-{
-	if (size == S_LONG)
-	{
-		return (n);
-	}
-	else if (size == S_SHORT)
-	{
-		return ((short)n);
-	}
-	else
-	{
-		return ((int)n);
-	}
-}
-/**
- * convert_size_unsigned- casts number to a specified size
- * @n: casted number
- * @size: type to be casted
- * Return: casted value
- */
-long int convert_size_unsigned(unsigned long int n, int size)
-{
-	if (size == S_LONG)
-	{
-		return (n);
-	}
-	else if (size == S_SHORT)
-	{
-		return ((unsigned short)n);
-	}
-	else
-	{
-		return ((unsigned int)n);
-	}
+
+	return (0);
 }
 
+/**
+ * convert_size_number - Casts a number to the specified size
+ * @num: Number to be casted.
+ * @size: Number indicating the type to be casted.
+ *
+ * Return: Casted value of num
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
 
+	return ((int)num);
+}
+
+/**
+ * convert_size_unsgnd - Casts a number to the specified size
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
+ *
+ * Return: Casted value of num
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
 
